b0d039bd2f32af6bf1ad7759066db875bc04b347
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-06-04 14:58:34 -0600

    adjusted seeds for postgres naming convention

--- db/seeds/04_pets_users.js
+++ db/seeds/04_pets_users.js
@@ -6,41 +6,41 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('pet_user').insert([
         {
-          pet_id: knex('users').where('name', 'Isaac').select('id'),
-          user_id: knex('pet').where('name', 'Cyrus').select('id')
+          pet_id: knex('pet').where('name', 'Cyrus').select('id'),
+          user_id: knex('users').where('name', 'Isaac').select('id')
         },
-        // {
-        //   pet_id: knex('users').where('name', 'Isaac').select('id'),
-        //   user_id: knex('pet').where('name', 'Mr. Meowmers').select('id')
-        // },
-        // {
-        //   pet_id: knex('users').where('name', 'Randy').select('id'),
-        //   user_id: knex('pet').where('name', 'Mr. Meowmers').select('id')
-        // },
-        // {
-        //   pet_id: knex('users').where('name', 'Randy').select('id'),
-        //   user_id: knex('pet').where('name', 'Sam').select('id')
-        // },
-        // {
-        //   pet_id: knex('users').where('name', 'Bob').select('id'),
-        //   user_id: knex('pet').where('name', 'Greg').select('id')
-        // },
-        // {
-        //   pet_id: knex('users').where('name', 'Bob').select('id'),
-        //   user_id: knex('pet').where('name', 'Darrel').select('id')
-        // },
-        // {
-        //   pet_id: knex('users').where('name', 'Beyonce').select('id'),
-        //   user_id: knex('pet').where('name', 'Barry').select('id')
-        // },
-        // {
-        //   pet_id: knex('users').where('name', 'Beyonce').select('id'),
-        //   user_id: knex('pet').where('name', 'Niles').select('id')
-        // },
-        // {
-        //   pet_id: knex('users').where('name', 'Sue').select('id'),
-        //   user_id: knex('pet').where('name', 'Rufus').select('id')
-        // }
+        {
+          pet_id: knex('pet').where('name', 'Mr. Meowmers').select('id'),
+          user_id: knex('users').where('name', 'Isaac').select('id')
+        },
+        {
+          pet_id: knex('pet').where('name', 'Mr. Meowmers').select('id'),
+          user_id: knex('users').where('name', 'Randy').select('id')
+        },
+        {
+          pet_id: knex('pet').where('name', 'Sam').select('id'),
+          user_id: knex('users').where('name', 'Randy').select('id')
+        },
+        {
+          pet_id: knex('pet').where('name', 'Greg').select('id'),
+          user_id: knex('users').where('name', 'Bob').select('id')
+        },
+        {
+          pet_id: knex('pet').where('name', 'Darrel').select('id'),
+          user_id: knex('users').where('name', 'Bob').select('id')
+        },
+        {
+          pet_id: knex('pet').where('name', 'Barry').select('id'),
+          user_id: knex('users').where('name', 'Beyonce').select('id')
+        },
+        {
+          pet_id: knex('pet').where('name', 'Niles').select('id'),
+          user_id: knex('users').where('name', 'Beyonce').select('id')
+        },
+        {
+          pet_id: knex('pet').where('name', 'Rufus').select('id'),
+          user_id: knex('users').where('name', 'Sue').select('id')
+        }
       ]);
     });
 };
